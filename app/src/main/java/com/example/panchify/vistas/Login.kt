c1f9a16a9d362aaee3c4bd70d6a8866c48b665ae
KabesitaKiwi <erwinlagos0506gmail.com>
2026-01-14 12:10:09 +0100

    Finaliza layout de login y añade recursos (background, color e imágenes)

--- app/src/main/java/com/example/panchify/vistas/Login.kt
+++ app/src/main/java/com/example/panchify/vistas/Login.kt
@@ -1,25 +1,21 @@
-package com.example.panchify
+package com.example.panchify.vistas
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.panchify.vistas.Login
+import com.example.panchify.R
 
-class MainActivity : AppCompatActivity() {
+class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+            insets 
         }
-        val intent = Intent(this, Login::class.java)
-        startActivity(intent)
-        finish()
     }
 }
