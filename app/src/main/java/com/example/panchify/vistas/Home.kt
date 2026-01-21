26175e925a1bfcf57951cf61a10ad505bec9bd07
HOME <erwinlagos0506@gmail.com>
2026-01-21 01:34:13 +0100

    modificacion de interfaz, botones de navegacion(temporales) y margenes

--- app/src/main/java/com/example/panchify/vistas/Home.kt
+++ app/src/main/java/com/example/panchify/vistas/Home.kt
@@ -1,11 +1,13 @@
 package com.example.panchify.vistas
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.panchify.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +18,43 @@ class Home : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_home -> {
+                    val intent = Intent(this, Home::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.nav_songs -> {
+                    val intent = Intent(this, Songs::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.nav_stats -> {
+                    // acción search
+                    val intent = Intent(this, Stats::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.nav_comments -> {
+                    // acción library
+                    val intent = Intent(this, Comments::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.nav_friends -> {
+                    // acción profile
+                    val intent = Intent(this, Friends::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+        }
+
     }
 }
